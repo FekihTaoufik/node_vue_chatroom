be3487ecedd64ff892a7a5c33a06ae4762f3142f
Taoufik Fekih <tafekih@outlook.fr>
2019-09-25 17:11:25 +0200

    added AddRoom + Chat + Room vue files

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
-
+import VueChatScroll from 'vue-chat-scroll'
+Vue.use(VueChatScroll)
 Vue.config.productionTip = false
 
 new Vue({
